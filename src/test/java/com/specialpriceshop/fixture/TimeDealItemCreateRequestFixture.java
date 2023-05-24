804b507dc3e1050de4cfdbd3f881c8824f9a858f
devksh930 <ksh930@icloud.com>
2023-05-24 23:14:22 +0900

    test : TimeDealItemCreateRequest, TimeDealTimeInfo 테스트 추가

---
+++ src/test/java/com/specialpriceshop/fixture/TimeDealItemCreateRequestFixture.java
@@ -0,0 +1,48 @@
+package com.specialpriceshop.fixture;
+
+import com.specialpriceshop.dto.StockCreateRequest;
+import com.specialpriceshop.dto.TimeDealItemCreateRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class TimeDealItemCreateRequestFixture {
+
+    public static TimeDealItemCreateRequest createTimedealItemCreateRequest(
+        final String itemName,
+        final String itemDescription,
+        final double itemOriginalPrice,
+        final double timeDealPrice,
+        final LocalDateTime timeDealStartDate,
+        final LocalDateTime timeDealEndDate,
+        final List<StockCreateRequest> stocks
+    ) {
+        return new TimeDealItemCreateRequest(
+            itemName,
+            itemDescription,
+            itemOriginalPrice,
+            timeDealPrice,
+            timeDealStartDate,
+            timeDealEndDate,
+            stocks
+        );
+    }
+
+    public static TimeDealItemCreateRequest createTimedealItemCreateRequest(
+        final String itemName,
+        final String itemDescription,
+        final double itemOriginalPrice,
+        final double timeDealPrice,
+        final List<StockCreateRequest> stocks
+    ) {
+        return new TimeDealItemCreateRequest(
+            itemName,
+            itemDescription,
+            itemOriginalPrice,
+            timeDealPrice,
+            LocalDateTime.now(),
+            LocalDateTime.now().plusDays(3L),
+            stocks
+        );
+    }
+
+}
