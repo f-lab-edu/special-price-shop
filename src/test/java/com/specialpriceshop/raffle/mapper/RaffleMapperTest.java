2cce4b4b60d42a9b614b29cd8904441ff4b589bb
devksh930 <ksh930@icloud.com>
2023-07-21 12:39:28 +0900

    test: Mapper test 추가

--- src/test/java/com/specialpriceshop/raffle/mapper/RaffleMapperTest.java
+++ src/test/java/com/specialpriceshop/raffle/mapper/RaffleMapperTest.java
@@ -1,23 +1,33 @@
-package com.specialpriceshop.raffle.dto;
+package com.specialpriceshop.raffle.mapper;
 
-import static com.specialpriceshop.fixture.raffle.RaffleItemCreateRequestFixture.createRaffleItemCreateRequest;
 import static com.specialpriceshop.fixture.StockCreateRequestFixture.createStockCreateRequest;
+import static com.specialpriceshop.fixture.raffle.RaffleItemCreateRequestFixture.createRaffleItemCreateRequest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.specialpriceshop.item.dto.StockCreateRequest;
+import com.specialpriceshop.item.mapper.StockMapper;
 import com.specialpriceshop.raffle.domain.Raffle;
+import com.specialpriceshop.raffle.dto.RaffleItemCreateRequest;
 import java.math.BigDecimal;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class RaffleItemCreateRequestTest {
+class RaffleMapperTest {
+
+    private RaffleMapper raffleMapper;
+
+    @BeforeEach
+    void init() {
+        raffleMapper = new RaffleMapper(new StockMapper());
+    }
 
     @Test
     @DisplayName("StockCreateRequest가 null인 경우")
     void toEntity_stockCreateRequestNull() {
-        final RaffleItemCreateRequest raffleItemCreateRequest = createRaffleItemCreateRequest(
+        final RaffleItemCreateRequest request = createRaffleItemCreateRequest(
             "상품명",
             "상품설명",
             BigDecimal.valueOf(5000),
@@ -25,7 +35,7 @@ class RaffleItemCreateRequestTest {
             null
         );
 
-        final Raffle entity = raffleItemCreateRequest.toEntity();
+        final Raffle entity = raffleMapper.createRequestToEntity(request);
 
         assertEquals(0, entity.getItem().getStocks().size());
         assertNotNull(entity.getItem().getStocks());
@@ -40,7 +50,7 @@ class RaffleItemCreateRequestTest {
             createStockCreateRequest("옵션", 10L, BigDecimal.valueOf(30000.0))
 
         );
-        final RaffleItemCreateRequest raffleItemCreateRequest = createRaffleItemCreateRequest(
+        final RaffleItemCreateRequest request = createRaffleItemCreateRequest(
             "상품명",
             "상품설명",
             BigDecimal.valueOf(5000),
@@ -48,7 +58,7 @@ class RaffleItemCreateRequestTest {
             stockCreateRequest
         );
 
-        final Raffle entity = raffleItemCreateRequest.toEntity();
+        final Raffle entity = raffleMapper.createRequestToEntity(request);
 
         assertNotNull(entity.getItem().getStocks());
     }
--- src/test/java/com/specialpriceshop/timedeal/mapper/TimeDealMapperTest.java
+++ src/test/java/com/specialpriceshop/timedeal/mapper/TimeDealMapperTest.java
@@ -1,4 +1,4 @@
-package com.specialpriceshop.timedeal.dto;
+package com.specialpriceshop.timedeal.mapper;
 
 import static com.specialpriceshop.fixture.StockCreateRequestFixture.createStockCreateRequest;
 import static com.specialpriceshop.fixture.timedeal.TimeDealItemCreateRequestFixture.createTimedealItemCreateRequest;
@@ -6,25 +6,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.specialpriceshop.item.dto.StockCreateRequest;
+import com.specialpriceshop.item.mapper.StockMapper;
 import com.specialpriceshop.timedeal.domain.TimeDeal;
+import com.specialpriceshop.timedeal.dto.TimeDealItemCreateRequest;
 import java.math.BigDecimal;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class TimeDealItemCreateRequestTest {
+class TimeDealMapperTest {
+
+    private TimeDealMapper timeDealMapper;
+
+    @BeforeEach
+    void init() {
+        timeDealMapper = new TimeDealMapper(new StockMapper());
+    }
 
     @Test
     @DisplayName("StockCreateRequest가 null인 경우")
     void toEntity_stockCreateRequestNull() {
-        final TimeDealItemCreateRequest timedealItemCreateRequest = createTimedealItemCreateRequest(
+        final TimeDealItemCreateRequest request = createTimedealItemCreateRequest(
             "상품명",
             "상품설명",
             BigDecimal.valueOf(5000),
             BigDecimal.valueOf(1000.0),
             null);
 
-        final TimeDeal entity = timedealItemCreateRequest.toEntity();
+        final TimeDeal entity = timeDealMapper.createRequestToEntity(request);
 
         assertEquals(0, entity.getItem().getStocks().size());
         assertNotNull(entity.getItem().getStocks());
@@ -39,14 +49,14 @@ class TimeDealItemCreateRequestTest {
             createStockCreateRequest("옵션", 10L, BigDecimal.valueOf(30000.0))
         );
 
-        final TimeDealItemCreateRequest timedealItemCreateRequest = createTimedealItemCreateRequest(
+        final TimeDealItemCreateRequest request = createTimedealItemCreateRequest(
             "상품명",
             "상품설명",
             BigDecimal.valueOf(5000),
             BigDecimal.valueOf(1000.0),
             stockCreateRequest);
 
-        final TimeDeal entity = timedealItemCreateRequest.toEntity();
+        final TimeDeal entity = timeDealMapper.createRequestToEntity(request);
 
         assertNotNull(entity.getItem().getStocks());
     }
